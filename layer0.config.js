4237cd36cc5546bf563f3e5db182c1b20976273c
kusergiy <kustovskuysergiy@yandex.ua>
2022-03-04 11:47:30 +0200

    Layer0 Starter on progress

--- layer0.config.js
+++ layer0.config.js
@@ -1,11 +1,13 @@
 module.exports = {
-  routes: './src/routes.ts',
-  connector: '@layer0/starter',
+  team: "kuserhii",
+  name: "kustarter-study-project.com",
+  routes: "./src/routes.ts",
+  connector: "@layer0/starter",
   backends: {
     origin: {
-      domainOrIp: process.env.DOMAIN_OR_IP || 'www.lushusa.com',
-      hostHeader: process.env.HOST_HEADER || 'www.lushusa.com',
+      domainOrIp: 'www.lushusa.com',
+      hostHeader: 'www.lushusa.com',
       // disableCheckCert: true // disable this line before goin to production
     },
   },
-}
+};
